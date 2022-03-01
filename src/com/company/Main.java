c0bda0cad7a1ddd8d7ac5d2631b690096eb72a21
emil-KEA <emil33v4@stud.kea.dk>
2022-03-01 10:19:45 +0100

    test code go() removed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    new Main().go();
-    }
-    public void go(){
-        System.out.println(convertCharToInt('G'));
-        System.out.println(convertIntTOChar(convertCharToInt('B')));
     }
     public char[] charArray = {' ','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z','Æ','Ø','Å'};
 
