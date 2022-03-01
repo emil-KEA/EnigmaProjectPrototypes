f41db679bb13774b8f8ba50d8f5073fdaa9e34ae
emil-KEA <emil33v4@stud.kea.dk>
2022-03-01 16:19:52 +0100

    With messageToInts() and cryptWithShift()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         new Main().go();
+
+    }
+    public void go(){
+        System.out.println(messageToInts("HELLO"));
     }
 
     public String alphabet = " ABCDEFGHIJKLMNOPQRSTUVXYZÆØÅ";
@@ -18,5 +22,16 @@ public class Main {
 
     public char convertIntTOChar(int integer){
         return alphabet.charAt(integer);
+    }
+    public int[] messageToInts(String message){
+        int[] messageInt = new int[message.length()];
+        for (int i = 0; i < message.length(); i++) {
+            messageInt[i] += convertCharToInt(message.charAt(i));
+        } return messageInt;
+    }
+    public int[] cryptWithShift(int key, int[] messageInt){
+        for (int i = 0; i < messageInt.length; i++) {
+            messageInt[i] += key;
+        }       return messageInt;
     }
 }
