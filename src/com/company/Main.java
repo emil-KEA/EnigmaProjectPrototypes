dec46d1cb59aeaffd40c88d9abb41c782022c044
emil-KEA <emil33v4@stud.kea.dk>
2022-03-01 10:16:36 +0100

    convertCharTOInt and convertIntTOChar added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    new Main().go();
+    }
+    public void go(){
+        System.out.println(convertCharToInt('G'));
+        System.out.println(convertIntTOChar(convertCharToInt('B')));
+    }
+    public char[] charArray = {' ','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z','Æ','Ø','Å'};
+
+    public int convertCharToInt(char character){
+        for(int i = 0; i < charArray.length; i++){
+            if(character == charArray[i]){
+                return i;
+            }
+        } return -1;
+    }
+
+    public char convertIntTOChar(int integer){
+        return charArray[integer];
     }
 }
