fa47de6dae8a3eeb71084d2f32717fd3df3bc187
emil-KEA <emil33v4@stud.kea.dk>
2022-03-02 10:56:22 +0100

    printEncryptOrDecrypt, chooseEncryptOrDecryptCaesar, getEncryptionMessage, getShiftKey added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     Scanner in = new Scanner(System.in);
+    String message;
+    int shiftKey;
 
     public static void main(String[] args) {
         new Main().go();
@@ -37,6 +39,36 @@ public class Main {
             default:
                 go();
         }
+    }
+    public void printEncryptOrDecrypt(){
+        System.out.println("Do you want to encrypt or decrypt a message or exit to main menu? \n" +
+                "1. enter \"1\" to ENCRYPT \n\"" +
+                "2. enter \"2\" to DECRYPT \n\"" +
+                "3. enter \"3\" to EXIT TO MAIN MENU\n");
+    }
+    public void chooseEncryptOrDecryptCaesar(){
+        int choice = in.nextInt();
+        switch (choice) {
+            case 1:
+                runCaesarEncryption();
+                break;
+            case 2:
+                runCaesarDecryption();
+                break;
+            default:
+                go();
+        }
+    }
+    public void getEncryptionMessage(){
+        System.out.print("Enter the message you want to encrypt: ");
+        getMessage();
+    }
+    public void getMessage(){
+        message = in.nextLine().toUpperCase();
+    }
+    public void getShiftKey(){
+        System.out.print("Enter shift key (between 1 and 29) : ");
+        shiftKey = in.nextInt();
     }
 
     public String alphabet = " ABCDEFGHIJKLMNOPQRSTUVXYZÆØÅ ABCDEFGHIJKLMNOPQRSTUVXYZÆØÅ";
