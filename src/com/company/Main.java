99dc9f576428a05d9ec0519028c503741556b7c9
emil-KEA <emil33v4@stud.kea.dk>
2022-03-02 10:38:56 +0100

    printMainMenu and chooseCipher added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,44 +1,82 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
         new Main().go();
 
     }
-    public void go(){
+
+    public void go() {
         executeTest();
+    }
+
+    public void printMainMenu() {
+        System.out.println("Welcome to EMIL'S ENIGMA MACHINE \n " +
+                "What do you want to do? \n" +
+                "1. enter \"1\" to use CAESAR CIPHER \n" +
+                "2. enter \"2\" to use VIGENÈRE CIPHER \n" +
+                "3. enter \"3\" to EXIT \n");
+    }
+
+    public void chooseCipher() {
+        int choice = in.nextInt();
+        switch (choice) {
+            case 1:
+                runCaesarCiper();
+                break;
+            case 2:
+                runVigenèreCipher();
+                break;
+            case 3:
+                exit();
+                break;
+            default:
+                go();
+        }
     }
 
     public String alphabet = " ABCDEFGHIJKLMNOPQRSTUVXYZÆØÅ ABCDEFGHIJKLMNOPQRSTUVXYZÆØÅ";
 
-    public int convertCharToInt(char character){
-       return alphabet.indexOf(character);
+    public int convertCharToInt(char character) {
+        return alphabet.indexOf(character);
     }
 
-    public char convertIntTOChar(int integer){
+    public char convertIntToChar(int integer) {
         return alphabet.charAt(integer);
     }
-    public int[] messageToInts(String message){
+
+    public int[] messageToInts(String message) {
         int[] messageInt = new int[message.length()];
         for (int i = 0; i < message.length(); i++) {
             messageInt[i] += convertCharToInt(message.charAt(i));
-        } return messageInt;
+        }
+        return messageInt;
     }
-    public int[] addShiftKey(int key, int[] messageInt){
+
+    public int[] addShiftKey(int key, int[] messageInt) {
         for (int i = 0; i < messageInt.length; i++) {
             messageInt[i] += key;
-        }       return messageInt;
-    }
-    public String convertIntArrayToString(int[] encryptedInt){
-        String encryptedMessage = "";
-        for (int i = 0; i < encryptedInt.length; i++) {
-            encryptedMessage += convertIntTOChar(encryptedInt[i]);
+            if (messageInt[i] > 29) {
+                messageInt[i] -= 29;
+            }
         }
-        return encryptedMessage;
+        return messageInt;
     }
-    public void executeTest(){
-        System.out.println(convertIntArrayToString(addShiftKey(28,messageToInts("HEJSA"))));
+
+    public String convertIntArrayToString(int[] encryptedInt) {
+        StringBuilder encryptedMessage = new StringBuilder();
+        for (int i = 0; i < encryptedInt.length; i++) {
+            encryptedMessage.append(convertIntToChar(encryptedInt[i]));
+        }
+        return encryptedMessage.toString();
+    }
+
+    public void executeTest() {
+        System.out.println(convertIntArrayToString(addShiftKey(28, messageToInts("HEJSA"))));
 
     }
 
